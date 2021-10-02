a1378b42fd771daaa8e2cdcdcc9384105381b9b4
Andrew Cook <patroller161@gmail.com>
2021-10-02 03:05:17 -0600

    save and load update
    boolean from fstream error to be fixed later

--- clicker_game.cpp
+++ clicker_game.cpp
@@ -1,6 +1,7 @@
-#include <windows.h>
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 int main()
@@ -17,10 +18,35 @@ int main()
 			if (x == "help.exe") {
 				cout << "click.exe -- cost: free!\n"
 					"upgrade.exe -- cost: starts at five clicks\n"
-					"autoclicker.exe -- cost: 100 clicks\n"
+					"-install autoclicker.exe -- cost: 100 clicks\n"
 					"superupgrade.exe -- cost: starts at 1,000 clicks\n"
-					"shortcut.exe -- cost: 10,000 clicks (type -c, -u, -s, or -m)\n"
-					"megaupgrade.exe -- cost: starts at 100,000 clicks\n";
+					"-install shortcut.exe -- cost: 10,000 clicks (type -c, -u, -s, or -m)\n"
+					"megaupgrade.exe -- cost: starts at 100,000 clicks\n"
+					"save.exe -- cost: free!\n"
+					"load.exe -- cost: free!\n";
+			}
+			if (x == "q" || x == "quit" || x == "exit") {
+				break;
+			}
+			if (x == "save.exe") {
+				ofstream saveFile("save.txt");
+				if (autoclick) {
+					monies += 100;
+				}
+				if (shortcut) {
+					monies += 10000;
+				}
+				saveFile << monies;
+				saveFile << "\n";
+				saveFile << upgrade;
+				saveFile.close();
+				if (autoclick) {
+					monies -= 100;
+				}
+				if (shortcut) {
+					monies -= 10000;
+				}
+				cout << "currently there is a bug with saving, autoclicker.exe and shortcut.exe will be refunded on save.\n";
 			}
 			if (x == "click.exe") {
 				monies += upgrade;
@@ -52,17 +78,16 @@ int main()
 				monies -= 100000 + upgrade;
 				upgrade += 1000;
 			}
-			if (x == "autoclicker.exe" && !autoclick && monies >= 100) {
+			if (x == "-install autoclicker.exe" && !autoclick && monies >= 100) {
 				monies -= 100;
 				autoclick = true;
 			}
-			if (x == "shortcut.exe" && !shortcut && monies >= 10000) {
+			if (x == "-install shortcut.exe" && !shortcut && monies >= 10000) {
 				monies -= 10000;
 				shortcut = true;
 			}
 			if (autoclick) {
 				if (x.empty()) {
-					Sleep(300);
 					cout << "C:/autoclicker.exe ran in the background\n";
 					monies += upgrade;
 				}
@@ -75,6 +100,27 @@ int main()
 				cout << "Stack Overflow! You win!!!";
 				cin.ignore();
 				break;
+			}if (x == "load.exe") {
+				string output;
+				int i = 0;
+				ifstream loadFile("save.txt");
+				stringstream stringholder;
+				while (getline(loadFile, output)) {
+					switch (i) {
+					case 0:
+						stringholder << output;
+						stringholder >> monies;
+						break;
+					case 1:
+						stringholder << output;
+						stringholder >> upgrade;
+						break;
+					default:
+						break;
+					}
+				}
+				loadFile.close();
+				cout << "Remember to re-purchase autoclicker.exe and shortcut.exe!\n";
 			}
 			cout << "clicks: ";
 			cout << monies;
